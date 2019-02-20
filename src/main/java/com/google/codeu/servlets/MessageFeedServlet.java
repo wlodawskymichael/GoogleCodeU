d25d31d68c24e7225ef3cf503c19a5759cb31ada
Michael Wlodawsky <wlodawskymichael@github.com>
2019-02-20 16:44:17 -0600

    added datastore connection to pull all messages from users

--- src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
@@ -1,11 +1,16 @@
 package com.google.codeu.servlets;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.codeu.data.Datastore;
+import com.google.codeu.data.Message;
+import com.google.gson.Gson;
 
 /**
  * Handles fetching all messages for public feed.
@@ -14,9 +19,25 @@ import javax.servlet.http.HttpServletResponse;
  @WebServlet("/feed")
  public class MessageFeedServlet extends HttpServlet {
 
+    private Datastore datastore;
+
+    public MessageFeedServlet() {
+        datastore = new Datastore();
+    }
+
+    /**
+     * Responds with a JSON response of all Message data for ALL Users
+     */
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse res) 
         throws IOException {
-            res.getOutputStream().println("This will be the message feed!");
+            res.setContentType("application/json");
+
+            List<Message> allMessages = datastore.getAllMessages();
+            Gson gson = new Gson();
+
+            String json = gson.toJson(allMessages);
+
+            res.getOutputStream().println(json);
     }
  }
