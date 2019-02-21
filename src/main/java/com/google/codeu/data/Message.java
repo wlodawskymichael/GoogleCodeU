f8002b0df8a7ac917408ae2f425f1cdf259b3c88
maggie1059 <maggie.wu1059@gmail.com>
2019-02-21 02:00:37 -0500

    steps 1-4, modified to take in recipient

--- src/main/java/com/google/codeu/data/Message.java
+++ src/main/java/com/google/codeu/data/Message.java
@@ -25,20 +25,28 @@ public class Message {
   private String user;
   private String text;
   private long timestamp;
+  private String recipient;
 
   /**
-   * Constructs a new {@link Message} posted by {@code user} with {@code text} content. Generates a
+   * Constructs a new {@link Message} posted by {@code user} with {@code text} content
+   * and sent to {@code recipient}. Generates a
    * random ID and uses the current system time for the creation time.
    */
-  public Message(String user, String text) {
-    this(UUID.randomUUID(), user, text, System.currentTimeMillis());
+  public Message(String user, String text, String recipient) {
+    this(UUID.randomUUID(), user, text, System.currentTimeMillis(), recipient);
   }
 
-  public Message(UUID id, String user, String text, long timestamp) {
+  public Message(UUID id, String user, String text, long timestamp, String
+  recipient) {
     this.id = id;
     this.user = user;
     this.text = text;
     this.timestamp = timestamp;
+    this.recipient = recipient;
+  }
+
+  public String getRecipient(){
+    return recipient;
   }
 
   public UUID getId() {
