95159b6d48fb25bb6e84db00a7822d02e0e3cedf
unknown <nikitam17@hotmail.com>
2019-03-11 00:36:29 -0500

    Modified user page.js

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -44,6 +44,8 @@ function showMessageForm() {
           messageForm.classList.remove('hidden');
         }
       });
+      
+      document.getElementById('about-me-form').classList.remove('hidden');
 }
 
 /** Fetches messages and add them to the page. */
@@ -66,6 +68,24 @@ function fetchMessages() {
         });
       });
 }
+
+/**About Me*/
+
+function fetchAboutMe(){
+  const url = '/about?user=' + parameterUsername;
+  fetch(url).then((response) => {
+    return response.text();
+  }).then((aboutMe) => {
+    const aboutMeContainer = document.getElementById('about-me-container');
+    if(aboutMe == ''){
+      aboutMe = 'This user has not entered any information yet.';
+    }
+    
+    aboutMeContainer.innerHTML = aboutMe;
+
+  });
+}
+
 
 /**
  * Builds an element that displays the message.
@@ -95,4 +115,5 @@ function buildUI() {
   setPageTitle();
   showMessageForm();
   fetchMessages();
+  fetchAboutMe();
 }
