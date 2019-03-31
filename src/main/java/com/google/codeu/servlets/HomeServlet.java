93ed0c4dc170ca956597b2e72a5ad3f2e7a95bc7
Michael Wlodawsky <wlodawskymichael@github.com>
2019-03-31 16:07:17 -0500

    changed the homepage to the updated and quicker jsp file

--- src/main/java/com/google/codeu/servlets/HomeServlet.java
+++ src/main/java/com/google/codeu/servlets/HomeServlet.java
@@ -30,7 +30,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Redirects the user to the Google login page or their page if they're already logged in.
  */
-@WebServlet("/home")
+@WebServlet("/")
 public class HomeServlet extends HttpServlet {
 
   @Override
