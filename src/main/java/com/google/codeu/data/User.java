7154aad79b37c7acd417bbc20f82ce987d207371
Michael Wlodawsky <wlodawskymichael@github.com>
2019-04-16 14:49:14 -0500

    added attributes to user class

--- src/main/java/com/google/codeu/data/User.java
+++ src/main/java/com/google/codeu/data/User.java
@@ -4,6 +4,9 @@ public class User {
 
   private String email;
   private String aboutMe;
+  private String age;
+  private int year;
+  private String username;
 
   public User(String email, String aboutMe) {
     this.email = email;
@@ -16,5 +19,17 @@ public class User {
 
   public String getAboutMe() {
     return aboutMe;
+  }
+
+  public String getAge() {
+    return age;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public String getUsername() {
+    return username;
   }
 }
